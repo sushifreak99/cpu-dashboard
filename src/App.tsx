33435b14738123569f431be10b2adf1cf1506531
sushifreak99 <boaz.ost.dev.24@gmail.com>
2023-08-14 01:00:26 +0300

    Add useRingBuffer hook
    Signed-off-by: sushifreak99 <boaz.ost.dev.24@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import fetch from 'cross-fetch'
 
 import './index.css'
 import './App.css'
+import type { CpuLoad } from './cpuLoad/types'
 import { usePolling } from './hooks/usePolling'
+import { useRingBuffer } from './hooks/useRingBuffer'
+import { BUFFER_SIZE, CPU_STATS_DELAY } from './utils/constants'
 
 const App = () => {
+  const [buffer, push] = useRingBuffer<CpuLoad>(BUFFER_SIZE)
   usePolling(() => {
     fetch('http://localhost:3000/api/stats')
       .then((res) => res.json())
-      .then(console.log)
-  }, 4000)
+      .then((cpuLoad: CpuLoad) => push(cpuLoad))
+  }, CPU_STATS_DELAY)
 
   return (
     <main className="App">
       <p>{process.env.VERSION}</p>
+      {buffer.map((cpuLoad) => (
+        <div key={cpuLoad.timestamp}>{cpuLoad.value}</div>
+      ))}
     </main>
   )
 }
