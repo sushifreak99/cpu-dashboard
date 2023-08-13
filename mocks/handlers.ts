f3ca48b7341479fdde531e9aa2f140777bd5a312
sushifreak99 <boaz.ost.dev.24@gmail.com>
2023-08-13 07:11:01 +0300

    Create a fake CPU stats API
    Signed-off-by: sushifreak99 <boaz.ost.dev.24@gmail.com>

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,8 +1,16 @@
 import { rest } from 'msw'
 
+import type { CpuLoad } from '../src/cpuLoad/types'
+
 export const handlers = [
-  rest.get('http://localhost:3000/api/stats', (req, res, ctx) => {
-    const data = {}
+  rest.get('http://localhost:3000/api/stats', (_req, res, ctx) => {
+    const data: CpuLoad = {
+      timestamp: new Date().getTime(),
+      value:
+        0.8 * 0.01 * Math.random() +
+        0.03 * Math.random() +
+        0.01 * Math.random(),
+    }
 
     return res(ctx.status(200), ctx.json(data))
   }),
